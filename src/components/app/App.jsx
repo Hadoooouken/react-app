dc08516cd4eac4dd0f8610a62e45a1f5163b6d4b
Filipp <wolverinexdd@yandex.ru>
2026-08-03 20:52:15 +0400

    employees list переписан на классовый компонент, добавлены динамические классы

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -8,9 +8,9 @@ import './App.css';
 
 function App() {
   const data = [
-    { name: 'Leon S', salary: 800, increase: false },
-    { name: 'Cris R', salary: 3000, increase: false },
-    { name: 'Jill V', salary: 5000, increase: true },
+    { name: 'Leon S', salary: 800, increase: false, id: 1 },
+    { name: 'Cris R', salary: 3000, increase: false, id: 2 },
+    { name: 'Jill V', salary: 5000, increase: true, id: 3 },
   ];
   return (
     <div className="App">
